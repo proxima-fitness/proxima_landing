526e979b55cea0d73185a548c2068be04368ace6
Jack Hanlon <jacklhanlon@yahoo.co.uk>
2023-11-19 00:02:37 -0800

    fix bug for img src

--- landing/src/custom.d.ts
+++ landing/src/custom.d.ts
@@ -1,8 +1,12 @@
+// declare module "*.svg" {
+//     const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+//     export default content;
+//   }
 declare module "*.svg" {
-    const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-    export default content;
-  }
-  
+  // This type should represent the source of the SVG file as a string
+  const content: string;
+  export default content;
+}
   declare module "*.png" {
     const content: string;
     export default content;
