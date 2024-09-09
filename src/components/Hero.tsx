46986f829010d2876275972304b6dd65c7b60c0e
Jack Hanlon <jacklhanlon@yahoo.co.uk>
2024-09-09 09:26:44 -0700

    remove extra app store buttons

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -36,7 +36,9 @@ const Hero = () => {
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
         A <span className="text-white">Dashboard</span> for your Personal Records.<p></p><p></p>Create, share and track Programs on our<span className="text-white"> Platform</span>. 
         </p>
-        <GetStarted />
+        <div>
+          <GetStarted />
+        </div>
       </div>
 
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
@@ -47,10 +49,6 @@ const Hero = () => {
         <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
         <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
         {/* gradient end */}
-      </div>
-
-      <div className={`ss:hidden ${styles.flexCenter}`}>
-        <GetStarted />
       </div>
     </section>
   );
