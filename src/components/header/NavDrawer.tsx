759657d3691a7f591cdbff6582089e0a3ca97b68
Jack Hanlon <jacklhanlon@yahoo.co.uk>
2024-09-17 20:08:08 -0700

    fixed mobile menu

---
+++ src/components/header/NavDrawer.tsx
@@ -0,0 +1,109 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { navLinks } from "../../assets/constants/index";
+import HomeIcon from '@mui/icons-material/Home';
+import TerminalIcon from '@mui/icons-material/Terminal';
+import FeaturedVideoIcon from '@mui/icons-material/FeaturedVideo';
+import LiveHelpIcon from '@mui/icons-material/LiveHelp';
+import LoginIcon from '@mui/icons-material/Login';
+import DownloadIcon from '@mui/icons-material/Download';
+import { menu } from "../../assets";
+
+type Anchor = 'top' | 'left' | 'bottom' | 'right';
+
+interface IProps {
+    setActive: (active: boolean) => void;
+ }
+
+export default function SwipeableTemporaryDrawer() {
+const [active, setActive] = React.useState("Home");
+
+  const [state, setState] = React.useState({
+    right: false,
+  });
+
+  const toggleDrawer =
+    (anchor: Anchor, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event &&
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return;
+      }
+
+      setState({ ...state, [anchor]: open });
+    };
+
+  const list = (anchor: Anchor) => (
+    <Box
+      sx={ {height: "100%", color: "white", background: `linear-gradient(
+        144.39deg,
+        #ffffff -278.56%,
+        #6d6d6d -78.47%,
+        #11101d 91.61%
+      );` , width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 } }
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {navLinks.map((link, index) => (
+          <ListItem key={ link.id } disablePadding>
+            <ListItemButton>
+            <ListItemIcon>
+                { link.id === "home" && <HomeIcon style={ { color: "#5ce1e6" } } /> }
+                { link.id === "programs" && <TerminalIcon style={ { color: "#5ce1e6" } } /> }
+                { link.id === "create-programs" && <FeaturedVideoIcon style={ { color: "#5ce1e6" } } /> }
+                { link.id === "faqs" && <LiveHelpIcon style={ { color: "#5ce1e6" } } /> }
+                { link.id === "sign-in" && <LoginIcon style={ { color: "#5ce1e6" } } /> }
+                { link.id === "cta" && <DownloadIcon style={ { color: "#5ce1e6" } } /> }
+              </ListItemIcon>
+                { link.id === "sign-in" ?
+                  <a href="https://app.proximafitness.com/">
+                    <ListItemText primary={ link.title } />
+                  </a>
+                :
+                  <a href={`#${link.id}`}>
+                  <ListItemText primary={ link.title } />
+                  </a>
+                }
+
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div>
+        <React.Fragment key={ "right" }>
+          <Button onClick={toggleDrawer("right", true)}>
+            <img
+                src={ menu }
+                alt="menu"
+                className="w-[28px] h-[28px] object-contain"
+                />
+          </Button>
+          <SwipeableDrawer
+            anchor={"right"}
+            open={state["right"]}
+            onClose={toggleDrawer("right", false)}
+            onOpen={toggleDrawer("right", true)}
+          >
+            {list("right")}
+          </SwipeableDrawer>
+        </React.Fragment>
+    </div>
+  );
+}
