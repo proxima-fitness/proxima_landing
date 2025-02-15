d98277eee8d8ecc55deace08ff8729435694b46f
Jack Hanlon <jacklhanlon@yahoo.co.uk>
2025-02-15 13:50:07 -0800

    progress on cleaning up folders

--- src/components/testimonials/Testimonials.tsx
+++ src/components/testimonials/Testimonials.tsx
@@ -5,35 +5,34 @@ import SimpleSlider from "../generic/SimpleSlider";
 import { motion } from "motion/react";
 import starGold from "../../../public/StarGold.svg";
 
-const Testimonials = () => (
-    <section
-        id="clients"
-        className={ `${styles.paddingY} ${styles.flexCenter} flex-col relative ` }
-        data-aos="fade-up"
-        data-aos-anchor-placement="top-bottom"
-        data-aos-once="true"
-    >
-        <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
-        <motion.div
-                initial={ { opacity: 0 } }
-                whileInView={ { opacity: 1 } }
-                viewport={ { once: true } }
-                className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
-            <h2 className={ styles.heading2 }>
-                <div className="flex md:flex-row flex-col justify-center items-center">
-                    <div className="flex flex-row justify-center items-center">
-                        4.5
-                        <img src={ (starGold as any).src } alt="star_gold" className="ml-2 mr-4 w-[50px] h-[40px] object-contain" />
+export const Testimonials: React.FC = () => {
+    return (
+        <section
+            id="clients"
+            className={ `${styles.paddingY} ${styles.flexCenter} flex-col relative ` }
+            data-aos="fade-up"
+            data-aos-anchor-placement="top-bottom"
+            data-aos-once="true"
+        >
+            <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
+            <motion.div
+                    initial={ { opacity: 0 } }
+                    whileInView={ { opacity: 1 } }
+                    viewport={ { once: true } }
+                    className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+                <h2 className={ styles.heading2 }>
+                    <div className="flex md:flex-row flex-col justify-center items-center">
+                        <div className="flex flex-row justify-center items-center">
+                            4.5
+                            <img src={ (starGold as any).src } alt="star_gold" className="ml-2 mr-4 w-[50px] h-[40px] object-contain" />
+                        </div>
+                        <div>
+                            on the App Store
+                        </div>
                     </div>
-                    <div>
-                        on the App Store
-                    </div>
-                </div>
-            </h2>
-        </motion.div>
-        <SimpleSlider />
-    </section>
-);
-
-export default Testimonials;
-
+                </h2>
+            </motion.div>
+            <SimpleSlider />
+        </section>
+    );
+};
--- src/components/visuals/FirstVisual.tsx
+++ src/components/visuals/FirstVisual.tsx
@@ -1,30 +1,30 @@
 import styles, { layout } from "../../style";
 import visualOne from "../../../public/visualOne.png";
 
-const FirstVisual = () => (
-    <section
-        id="first-visual"
-        data-aos="fade-up"
-        data-aos-anchor-placement="top-bottom"
-        data-aos-once="true"
-        className={ ` ${layout.section}` }
-    >
-        <div
-            className={ layout.sectionInfoReverse }
-            style={ {
-                backgroundImage: `url(${visualOne})`,
-                backgroundSize: "cover",
-                backgroundPosition: "top",
-                backgroundRepeat: "no-repeat",
-                borderRadius: "30px",
-                } }
+export const FirstVisual: React.FC = () => {
+    return (
+        <section
+            id="first-visual"
+            data-aos="fade-up"
+            data-aos-anchor-placement="top-bottom"
+            data-aos-once="true"
+            className={ ` ${layout.section}` }
         >
-            <h2 className={ `${styles.heading2End} ss:pr-44 ss:pt-32 italic white-gradient` }>
-                &quot;We take on the strength
-                <br className="sm:block hidden" /> of that which we overcome.&quot;
-            </h2>
-        </div>
-    </section>
-);
-
-export default FirstVisual;
+            <div
+                className={ layout.sectionInfoReverse }
+                style={ {
+                    backgroundImage: `url(${visualOne})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "top",
+                    backgroundRepeat: "no-repeat",
+                    borderRadius: "30px",
+                    } }
+            >
+                <h2 className={ `${styles.heading2End} ss:pr-44 ss:pt-32 italic white-gradient` }>
+                    &quot;We take on the strength
+                    <br className="sm:block hidden" /> of that which we overcome.&quot;
+                </h2>
+            </div>
+        </section>
+    );
+};
